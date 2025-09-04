680825d36cb0f812d8edb23a33434200a5ad65a8
Giovanna Caron <giovanna.c.barros@aluno.senai.br>
2025-09-04 13:48:30 -0300

    Feat:create folders and workspace

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const categoriaRoutes= require("./src/routes/categoriaRoutes");
+const profissionalRoutes = require("./src/routes/profissionalRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/categorias", categoriaRoutes);
+app.use("/api/profissionais", profissionalRoutes);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
