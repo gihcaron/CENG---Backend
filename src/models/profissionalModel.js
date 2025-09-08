f19e3167789f034222eaa19ae93a78a1a8fa1fc1
Giovanna <giovannacaron.tds1.senai@gmail.com>
2025-09-08 18:33:26 -0300

    Fix: sintax problems in getProfissionais method

--- src/models/profissionalModel.js
+++ src/models/profissionalModel.js
@@ -1,14 +1,23 @@
 const pool = require("../config/database");
 
 const getProfissionais = async (nome) => {
+    let result;
     if (nome) {
         result = await pool.query(
-            "SELECT * FROM profissionais WHERE nome ILIKE $1",
-            [`%${nome}%`]
-        );
+        `SELECT profissionais.*, categorias.nome AS categoria_nome
+        FROM profissionais
+        LEFT JOIN categorias ON profissionais.categoria_id = categorias.id
+        WHERE profissionais.nome ILIKE $1`,
+        [`%${nome}%`]);
+        return result.rows;
     } else {
-        result = await pool.query("SELECT * FROM profissionais");
+       result = await pool.query(
+            `SELECT profissionais.*, categorias.nome AS categoria_nome
+            FROM profissionais
+            LEFT JOIN categorias ON profissionais.categoria_id = categorias.id`
+       )
     }
+
     return result.rows;
 };
 
@@ -20,24 +29,19 @@ const getProfissionalById = async (id) => {
     return result.rows[0];
 };
 
-const createProfissional = async (
-    nome,
-    idade,
-    area_atuacao,
-    biografia,
-    redes_sociais,
-    foto,
-    pais,
-    categoria_id
-) => {
-    const result = await pool.query(
-        `INSERT INTO profissionais
-        (nome, idade, area_atuacao, biografia, redes_sociais, foto, pais, categoria_id)
-        VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *`,
+const createProfissional = async (nome, idade, area_atuacao, biografia, redes_sociais, foto, pais, categoria_id) => {
+    try{
+        const result = await pool.query(
+        "INSERT INTO profissionais (nome, idade, area_atuacao, biografia, redes_sociais, foto, pais, categoria_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
         [nome, idade, area_atuacao, biografia, redes_sociais, foto, pais, categoria_id]
-    );
-    return result.rows[0];
-};
+        );
+        return result.rows[0];
+        } catch (error) {
+        console.error("Erro ao criar profissional:", error);
+        throw error; 
+    }
+    };
+
 
 const updateProfissional = async (id, dados) => {
     const { nome, idade, area_atuacao, biografia, redes_sociais, foto, pais, categoria_id } = dados;
