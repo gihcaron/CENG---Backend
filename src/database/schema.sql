4ead39b74b7d5cb088d7efb3720137e86b6d993f
Giovanna Caron <giovanna.c.barros@aluno.senai.br>
2025-09-09 12:01:44 -0300

    FEAT: create profissionais

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -15,6 +15,10 @@ INSERT INTO categorias (id, nome, descricao, ano_inicio) VALUES
 (4, 'Copa Truck', 'Competição de caminhões no Brasil', 1985),
 (5, 'F1 Academy', 'Categoria de formação para jovens pilotos femininas', 2023),
 (6, 'Porsche Cup', 'Categoria de carros esportivos Porsche', 2003);
+(7, 'Turismo Nacional', 'Campeonato brasileiro de carros de turismo', 2000),
+(8, 'Porsche Cup Brasil', 'Categoria brasileira de carros esportivos Porsche', 2003);
+(9,'AMG Cup', 'Categoria brasileira de carros esportivos Mercedes-AMG', 2020);
+
 
 
 CREATE TABLE profissionais (
@@ -43,3 +47,5 @@ INSERT INTO profissionais (nome, idade, area_atuacao, biografia, redes_sociais, 
 ('Thaline Chicoski', 30, 'Piloto', 'Thaline Chicoski, pilota paranaense de Campo Mourão, descobriu sua paixão pelo automobilismo em 2010, quando decidiu experimentar o kart de seu irmão. Aos 14 anos iniciou sua trajetória, conquistando o bicampeonato paranaense de kart e se tornando a única mulher a vencer na Copa HB20, onde competiu por quatro temporadas. Atualmente, com 30 anos, representa a equipe DMais Motorsport na AMG Cup Brasil e na Copa Truck.', '@thalinechicoski', NULL, 'Brasil', 4);
 ('Erika Prado', NULL, 'Engenharia de Performance e Dados', 'Engenheira com experiência na Fórmula Vee, Copa HB20, Stock Light, GP do Galeão e Fórmula 4 Brasil. Atuou como Performance Engineer na Cavaleiro Sports e hoje é destaque na F4 Brasil. Fundadora do projeto Girls Like Racing, que aproxima e incentiva mulheres no automobilismo, impactando milhares de mulheres de todo o Brasil.', NULL, NULL, 'Brasil', 3);
 ('Carol Nunes', 30, 'Pilota', 'Paulista e primeira pilota trans do Brasil, Carol Nunes iniciou no automobilismo com um ousado projeto: transformar sozinha um Ford Fusion em carro de corrida, o que lhe rendeu notoriedade e vitórias na Subida de Montanha e na Super Liga Desportiva de Velocidade. Conquistou título na categoria feminina da liga e chegou a competir na Copa HB20, sempre enfrentando barreiras de patrocínio e preconceito, mas se consolidando como símbolo de diversidade e resistência no esporte a motor.', NULL, NULL, 'Brasil', 4);
+('Nome da Jornalista', NULL, 'Jornalista', 'Jornalista formada pela Universidade Presbiteriana Mackenzie, com experiência em veículos como Destak, eduK, Tazio, Grande Prêmio e na organização da Stock Car. Atualmente é editora no site F1 Mania, onde cobre Fórmula 1, Stock Car e Porsche Cup, além de já ter atuado em eventos internacionais como F1, Fórmula E, WEC e Indy. Criadora da página Elas na Pista, soma mais de 10 mil seguidores no YouTube, destacando-se na produção de conteúdo e na representatividade feminina no automobilismo.', '@elasnapista', NULL, 'Brasil', 7);
+('Bia Martins', 25, 'Piloto', 'Natural de João Pessoa (PB), Bia Martins começou no kart aos 9 anos e se destacou como a única mulher na F-400 do Nordeste. Em 2023 estreou nos carros pela HB20 Racing Cup, encerrando duas temporadas entre os sete primeiros colocados. Em 2025 estreia na Turismo Nacional pela equipe GS Sports, ao lado de Maria Luiza Bedin, acelerando o Chevrolet New Onix #55.', NULL, NULL, 'Brasil', 4);
